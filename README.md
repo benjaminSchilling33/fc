246e6e6365011c36e6f2488ee756dc836294fc7b
Benjamin Schilling <benjamin.schilling33@gmail.com>
2025-01-01 19:58:27 +0100

    feat: confirm

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ E.g. in a directory with many *.orf and *.jpg files all files that exist in eith
 This is useful when cleaning up photos by only going through all photos with one file extension, e.g. *.jpg, while deleting all unwanted ones and afterwards removing all corresponding RAW files as well.
 
 ```bash
-fc 0.1
+fc 0.2
 Benjamin Schilling <benjamin.schilling33@gmail.com>
 USAGE:
     fc.exe --extension <extensions>... --path <path>
@@ -27,4 +27,4 @@ SPDX-License-Identifier: MIT
 
 # Copyright
 
-Copyright (C) 2021 Benjamin Schilling
+Copyright (C) 2021 - 2025 Benjamin Schilling
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@
 */
 
 // Rust Standard Library
-use std::ffi::OsString;
 use std::fs;
 use std::fs::DirEntry;
 use std::path::Path;
+use std::{ffi::OsString, io};
 // CLI
 extern crate clap;
 use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("fc")
-        .version("0.1.0")
+        .version("0.2.0")
         .author("Benjamin Schilling <benjamin.schilling33@gmail.com>")
         .about("Among all files with the given file extensions, remove individual files.")
         .arg(
@@ -35,7 +35,17 @@ fn main() {
                 .multiple(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("confirm")
+                .short("c")
+                .takes_value(false)
+                .help("Immediately confirm deletion.")
+                .required(false),
+        )
         .get_matches();
+
+    // auto confirm?
+    let auto_confirm = matches.is_present("confirm");
 
     // read specified file extensions in scope of checking
     let extensions = matches.values_of("extensions").unwrap();
@@ -57,12 +67,43 @@ fn main() {
         .collect();
 
     let mut files_to_delete = Vec::new();
-
+    println!("Going to delete the following files:");
     for filename in &filenames {
         if filenames.iter().filter(|&n| n == filename).count() < 2 {
             files_to_delete.push(filename);
+            println!("{:?}", filename);
         }
     }
+    if files_to_delete.len() == 0 {
+        println!("No files matching extensions found.");
+        return;
+    }
+    if !auto_confirm {
+        println!("Confirm (y/Y):");
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        buffer = buffer.trim().to_string();
+        if buffer == "y" || buffer == "Y" {
+            delete(files_to_delete, extensions, file_path);
+            print!("Deleted files.")
+        }
+    } else {
+        delete(files_to_delete, extensions, file_path);
+    }
+}
+
+fn match_extension(file: &DirEntry, extensions: clap::Values) -> bool {
+    for ext in extensions {
+        if let Some(e) = file.path().extension() {
+            if e.eq_ignore_ascii_case(ext) {
+                return true;
+            };
+        }
+    }
+    return false;
+}
+
+fn delete(files_to_delete: Vec<&OsString>, extensions: clap::Values, file_path: &str) {
     // delete files
     for file in files_to_delete {
         for ext in extensions.clone() {
@@ -82,13 +123,37 @@ fn main() {
     }
 }
 
-fn match_extension(file: &DirEntry, extensions: clap::Values) -> bool {
-    for ext in extensions {
-        if let Some(e) = file.path().extension() {
-            if e.eq_ignore_ascii_case(ext) {
-                return true;
-            };
-        }
+#[cfg(test)]
+mod tests {
+    use std::process::Command;
+
+    use super::*;
+    use fs::File;
+
+    #[test]
+    fn good_case() {
+        // create directory
+        fs::create_dir("./test_data/").unwrap();
+        // create test files, one will be cleaned, the other stays
+        let _file = File::create("./test_data/foo.jpg").unwrap();
+        let _file = File::create("./test_data/bar.jpg").unwrap();
+        let _file = File::create("./test_data/bar.orf").unwrap();
+        print!("cleaning test_now");
+        // run fc for test directory with "alt" and "test" file ending
+        let mut fs_process = Command::new("target/release/fc")
+            .args(&["-p", "test_data", "-e", "jpg", "-e", "orf", "-c"])
+            .spawn()
+            .expect("Failed to start fc process.");
+        let _ = fs_process.wait();
+
+        let file_should_not_exist = match fs::metadata("./test_data/foo.jpg") {
+            Ok(o) => o.is_file(),
+            _ => false,
+        };
+        let file_shout_exist = fs::metadata("./test_data/bar.jpg").unwrap().is_file();
+
+        let _ = fs::remove_dir_all("./test_data/");
+
+        assert!(file_shout_exist && !file_should_not_exist);
     }
-    return false;
 }
